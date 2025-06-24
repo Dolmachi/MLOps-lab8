f7439b45016dcd6112eeaed67b97e0c1ad7902d8
Dolmachi <ernestkuznecov8@gmail.com>
2025-06-24 09:48:45 +0300

    final version

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import time
 import json
 import requests
 from logger   import Logger
@@ -28,8 +27,6 @@ class InferenceJob:
             self.current_partition += 1
             self.log.info(f"Отправлено {batch} порций")
         
-        self.log.info("Приостановка 5 минут!")
-        time.sleep(300)
         self.pred.stop()
         self.log.info("Работа завершена")
 
